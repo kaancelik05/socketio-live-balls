f8f7f22100ce6f85ab00f5da94c640f71c8dc831
kaan <k.celik05@gmail.com>
2018-09-09 00:01:46 +0300

    added chat

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,5 +1,8 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
 
+    $scope.messages = [];
+    $scope.players = {};
+
     $scope.init = () => {
         const username = prompt('Please enter username');
 
@@ -8,6 +11,20 @@ app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFacto
         else
             return false;
     };
+
+    function scrollTop() {
+        setTimeout(() => {
+            const element = document.getElementById('chat-area');
+            element.scrollTop = element.scrollHeight;
+        });
+    }
+
+    function showBubble(id, message){
+        $('#'+id).find('.message').show().html(message);
+        setTimeout(() => {
+            $('#'+id).find('.message').hide()
+        },2000);
+    }
 
     function initSocket(username) {
         const connectionOptions = {
@@ -18,6 +35,91 @@ app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFacto
         indexFactory.connectSocket('http://localhost:3000', connectionOptions)
             .then((socket) => {
                 socket.emit('newUser', { username });
+
+                socket.on('initPlayers', (players) => {
+                    $scope.players = players;
+                    $scope.$apply();
+                })
+
+                socket.on('newUser', (data) => {
+                    const messageData = {
+                        type: {
+                            code: 0, // server
+                            message: 1 // login
+                        },
+                        username: data.username
+                    };
+
+                    $scope.messages.push(messageData);
+                    $scope.players[data.id] = data;
+                    scrollTop()
+                    $scope.$apply();
+                });
+
+                socket.on('disUser', (data) => {
+                    console.log(data);
+                    const messageData = {
+                        type: {
+                            code: 0, //server
+                            message: 0 //disconnect
+                        },
+                        username: data.username
+                    };
+
+                    $scope.messages.push(messageData);
+                    delete $scope.players[data.id];
+                    scrollTop()
+                    $scope.$apply();
+                });
+
+                socket.on('animate', data => {
+                    console.log(data);
+                    $('#' + data.socketId).animate({ 'left': data.x, 'top': data.y }, () => {
+                        animate = false;
+                    });
+                });
+
+                socket.on('newMessage', message => {
+                    $scope.messages.push(message);
+                    $scope.$apply();
+                    showBubble(message.socketId, message.text)
+                    scrollTop();
+                });
+
+                let animate = false;
+                $scope.onClickPlayer = ($event) => {
+                    if (!animate) {
+                        let x = $event.offsetX;
+                        let y = $event.offsetY;
+
+                        socket.emit('animate', { x, y });
+                        animate = true;
+                        $('#' + socket.id).animate({ 'left': $event.offsetX, 'top': $event.offsetY }, () => {
+                            animate = false;
+                        });
+                    };
+                };
+
+                $scope.newMessage = () => {
+                    let message = $scope.message;
+                    console.log(message);
+
+                    const messageData = {
+                        type: {
+                            code: 1, // server
+                        },
+                        username: username,
+                        text: message
+                    };
+
+                    $scope.messages.push(messageData);
+                    $scope.message = '';
+
+                    socket.emit('newMessage', messageData);
+                    showBubble(socket.id, message)
+                    scrollTop();
+
+                }
             }).catch((err) => {
                 console.log(err)
             });
